ea4531da6f9bbad7653b0c4fd0acb5f6e7a1dbdc
Jazmin M <jazminmerckel@gmail.com>
2019-10-04 00:56:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,8 +38,8 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end 
-array = ["dog", "cat", "bird"]
-swap_elements(array)
+animals = ["dog", "cat", "bird"]
+swap_elements(animals)
   
   
   
